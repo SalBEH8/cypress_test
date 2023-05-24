740a03a82cd6b65936f2b0371acd739bdab0de8e
SalBEH8 <salembehatt@gmail.com>
2023-05-24 17:14:31 +0200

    correction

--- cypress/e2e/quest3.cy.js
+++ cypress/e2e/quest3.cy.js
@@ -4,7 +4,7 @@ const testData = require('../fixtures/testData.json');
 const uuid = () => Cypress._.random(0, 1e6); // Universally Unique Identifier vien avec Node
 const id = uuid(); // integree comme variable
 
-// Commande personnalisée pour la connexion
+// Custom logging
 Cypress.Commands.add('login', (email, password) => {
     cy.request('POST', 'https://practice.expandtesting.com/notes/api/users/login', {
         email,
@@ -13,6 +13,25 @@ Cypress.Commands.add('login', (email, password) => {
         expect(response.status).to.eq(200);
         expect(response.body.data.email).to.eq(email);
         expect(response.body.data).to.have.property('token');
+        return response;
+    });
+});
+
+// Custom command notes
+Cypress.Commands.add('createNote', (token, note) => {
+    cy.request({
+        method: 'POST',
+        url: 'https://practice.expandtesting.com/notes/api/notes',
+        headers: {
+            'x-auth-token': token,
+        },
+        body: note
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.data).to.have.property('id');
+        expect(response.body.data.title).to.eq(note.title);
+        expect(response.body.data.description).to.eq(note.description);
+        expect(response.body.data.category).to.eq(note.category);
     });
 });
 
@@ -24,9 +43,6 @@ describe('API tests', () => {
     });
 
     it('creates a new user', () => {
-
-        // permet de crée le random User et PSd
-        
         const newUser = { 
             name: `TestUser${id}`,
             email: `test${id}@example.com`,
@@ -40,7 +56,6 @@ describe('API tests', () => {
     });
 
     it('logs in a user', () => {
-        // réutilisé pour le relog
         const { email, password } = testData.userLogin;
         cy.login(email, password);
     });
@@ -54,6 +69,19 @@ describe('API tests', () => {
             failOnStatusCode: false
         }).then((response) => {
             expect(response.status).to.eq(401);
+        });
+    });
+
+    it('creates a new note', () => {
+        const { email, password } = testData.userLogin;
+        cy.login(email, password).then(response => {
+            const token = response.body.data.token;
+            const note = { 
+                title: `Test Note${id}`,
+                description: `Test Description${id}`,
+                category: 'Work'
+            };
+            cy.createNote(token, note);
         });
     });
 });
